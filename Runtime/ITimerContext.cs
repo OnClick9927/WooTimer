3c60a4c6f5d7dafd6a4d7bba1553f4bb1013d5b7
Ling Wen <54854040+OnClick9927@users.noreply.github.com>
2025-05-14 14:34:23 +0800

    提交

---
+++ Runtime/ITimerContext.cs
@@ -0,0 +1,23 @@
+﻿/*********************************************************************************
+ *Author:         OnClick
+ *Version:        0.0.2.116
+ *UnityVersion:   2018.4.24f1
+ *Date:           2020-11-29
+ *Description:    IFramework
+ *History:        2018.11--
+*********************************************************************************/
+namespace WooTimer
+{
+    public delegate bool TimerFunc(float time, float delta);
+    public delegate void TimerAction(float time, float delta);
+
+
+    public interface ITimerContext
+    {
+        string id { get; }
+        bool isDone { get; }
+        bool canceled { get; }
+
+    }
+
+}
